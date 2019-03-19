8b88f9a4cc36b9be2c43260ade5499d46d5b09ca
jsteng19 <35145360+jsteng19@users.noreply.github.com>
2019-03-19 08:33:45 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import json
+import importlib
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    db = json.loads(open("/Users/jstenger/Documents/workspace/remote_machine/database/programs.json").read())
+    print(db)
+    return render_template('home.html', db=map(json.dumps, db))
+
+
+@app.route('/run', methods=['GET', 'POST'])
+def run():
+    # return str(request.form.get("programs"))
+    return str(runFile("Scripts", str(request.form.get("program")), [9]))
+   
+    
+def runFile(location, name, args):
+    # for x in range len()
+    file = importlib.import_module(location + "." + name)
+    # print(file)
+    return file.main(*args)
+
+
+if __name__ == "__main__":
+    app.run()
