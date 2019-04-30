45d83511ee57c41d13144aa7feb162e62b8b15dd
jsteng19 <35145360+jsteng19@users.noreply.github.com>
2019-04-30 11:43:38 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-from flask import Flask, render_template, request
-import json, importlib, unittest, os
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+import json
+import importlib
+import os
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+
+UPLOAD_FOLDER = '/Users/jstenger/Documents/workspace/remote_machine2/Scripts'
+ALLOWED_EXTENSIONS = set(['py', 'jpg', 'png'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(app.instance_path, app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+
+            # return redirect(url_for('uploaded_file', filename=file.filename))
+    return render_template('upload.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -14,24 +54,7 @@ def run():
     return str(runFile("Scripts", str(request.form.get("program")), [int(request.form.get("input"))])), 200
 
     
-
-
-def runFile(name,*args):
-    index = os.path.join(os.path.dirname(__file__),'database/file_paths.json')
-    with open(index, 'r') as raw_file:
-        scriptStorage = json.loads(raw_file.read())
-        #print(scriptStorage[name])
-    try:
-        location = scriptStorage[name]["path"]
-        #print(location)
-    except KeyError:
-        return "Unexpected Error: The script is either missing or has an invalid location"
-    try:
-        file = importlib.import_module(location+"."+name) 
-        output = file.main(*args)
-    except Exception as excpt:
-        output = "Unexpected Error: "+str(excpt)
-    return output  
+def runFile(location, name, args):
     print(args);
     # for x in range len()
     file = importlib.import_module(location + "." + name)
@@ -39,14 +62,5 @@ def runFile(name,*args):
     return file.main(*args)
 
 
-
-class testRunFile(unittest.TestCase):
-    def testFibCorrect(self):
-        self.assertEqual(runFile('Fibonacci',2),1)
-        self.assertEqual(runFile('Fibonacci',23),28657)
-    def testFibErrors(self):
-        self.assertEqual(runFile('Fibonacci',2,3),'Unexpected Error: main() takes 1 positional argument but 2 were given')
-        self.assertEqual(runFile('FakeProgram',2),'Unexpected Error: The script is either missing or has an invalid location')
 if __name__ == "__main__":
     app.run()
-
